78d1e94e8d12bb2aa501a1ff9d91805d599efc96
Giuliana Villarroel <giu.villarroel@gmail.com>
2026-03-28 16:22:45 -0300

    feat: implement Ship factory with TDD

--- tests/Ship.test.js
+++ tests/Ship.test.js
@@ -1,1 +1,29 @@
-test.todo("implemented test");
+import { Ship } from "../src/Ship";
+
+describe("Ship", () => {
+	test("has the correct length", () => {
+		const ship = Ship(3);
+		expect(ship.length).toBe(3);
+	});
+	test("starts with 0 hits", () => {
+		const ship = Ship(3);
+		expect(ship.hits).toBe(0);
+	});
+	test("hit() increments hits", () => {
+		const ship = Ship(3);
+		ship.hit();
+		expect(ship.hits).toBe(1);
+	});
+	test("isSunk() return false if not sunk", () => {
+		const ship = Ship(3);
+		ship.hit();
+		expect(ship.isSunk()).toBe(false);
+	});
+	test("isSunk() returns true if sunk", () => {
+		const ship = Ship(3);
+		ship.hit();
+		ship.hit();
+		ship.hit();
+		expect(ship.isSunk()).toBe(true);
+	});
+});
